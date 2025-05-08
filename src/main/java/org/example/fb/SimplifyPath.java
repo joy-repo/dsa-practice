bd7a5deef041d957f5293f1f12548177546eb67b
joy-repo <66773528+joy-repo@users.noreply.github.com>
2025-05-08 12:35:35 +0530

    Create SimplifyPath.java
    coomii

---
+++ src/main/java/org/example/fb/SimplifyPath.java
@@ -0,0 +1,36 @@
+
+
+
+import java.util.Stack;
+
+/// TODO: Leet 71
+public class SimplifyPath {
+
+  static String path = "/a/b/.././//./d";
+
+  public static void main(String[] args) {
+    solveLeet71();
+
+  }
+
+  private static void solveLeet71() {
+
+    String[] splitPath = path.split("/");
+    Stack<String> stack = new Stack<String>();
+    for (String folder : splitPath) {
+      if(folder.equals("")) continue;
+      if(folder.equals("..") && !stack.isEmpty()) {
+        stack.pop();
+      } else if (folder.equals(".")) {
+        continue;
+      } else {
+        stack.push(folder);
+      }
+
+    }
+    String result = "/"+String.join("/", stack);
+    System.out.println(result);
+
+  }
+
+}
